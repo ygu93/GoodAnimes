606cea27481d2d1233a1f33f69eeb75aceb1f2b2
Yue Gu <yuegu1993@gmail.com>
2016-11-04 01:38:56 -0700

    improve splash page

--- frontend/components/greeting/greeting.jsx
+++ frontend/components/greeting/greeting.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import {Link} from 'react-router';
+import Scroll from 'react-scroll';
+const Element = Scroll.Element;
+const scroller = Scroll.scroller;
 
 
 class Greeting extends React.Component{
   constructor(props){
     super(props);
   }
+
+  scrollToDesc(){
+    return scroller.scrollTo('scroll-target', {smooth:true});
+  }
+
 
   render(){
     let currentUser = this.props.currentUser;
@@ -19,24 +27,35 @@ class Greeting extends React.Component{
     else{
       return(
       <div className = 'splash'>
-        <video id="background-video" loop autoPlay preload>
-        <source src="https://s3.amazonaws.com/goodanimesvideos/splash.mp4" />
-        Your browser does not support this video.
-        </video>
+        <div className='hero-section'>
+          <video id="background-video" loop autoPlay preload>
+          <source src="https://s3.amazonaws.com/goodanimesvideos/splash.mp4" />
+          Your browser does not support this video.
+          </video>
+          <div className="hero-overlay"></div>
+          <div className='intro'>
+            <p className='watermark'><span className='logo1'>good</span><span className='logo2'>animes</span></p>
+            <p className='intro-message'>Share your anime experiences with others, find new animes, and track your favorite animes</p>
+            <p className='learn-more' onClick={this.scrollToDesc}>Learn More</p>
+          </div>
+        </div>
+      <Element name='scroll-target'></Element>
+      <div className='about'>
         <h2 className ='welcome'>Meeting your next favorite Anime</h2>
         <section className = 'descr'>
-        <p>
-          Finding the right anime to watch is half the battle when watching anime. With goodanimes, you can always find the right
-          anime for you.
-        </p>
+          <p>
+            When watching anime, finding the right anime to watch is half the battle. With goodanimes, you can always find the right
+            anime for you.
+          </p>
 
-        <p>
-          Browse an anime page to read a detailed synopsis, see the average rating and read reviews on the show.
-        </p>
-        <p>
-          Build your collection by keeping track of your shows in libraries including your own custom libraries
-        </p>
+          <p>
+            Browse an anime's detail page to read a detailed synopsis, see the average rating and read reviews on the show.
+          </p>
+          <p>
+            Build your collection by keeping track of all your shows in custom libraries
+          </p>
         </section>
+      </div>
       </div>
     );
     }
