3b4d76dea953e372a77e61376f1747296f545021
Yue Gu <yuegu1993@gmail.com>
2016-12-05 11:41:30 -0800

    remove minify version

---
+++ webpack.config.prod.js
@@ -0,0 +1,40 @@
+var webpack = require("webpack");
+
+const path = require('path');
+
+module.exports = {
+  context: __dirname,
+  entry: './frontend/good_animes.jsx',
+  output: {
+    path: path.join(__dirname, 'app', 'assets', 'javascripts'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  plugins:[
+   new webpack.DefinePlugin({
+     'process.env':{
+       'NODE_ENV': JSON.stringify('production')
+     }
+   }),
+   new webpack.optimize.UglifyJsPlugin({
+     compress:{
+       warnings: true
+     }
+   })
+ ],
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
+  },
+  devtool: 'source-maps'
+};
