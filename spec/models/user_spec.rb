e978a47bc7544021450615eaf2f38c28080bcc9c
Yue Gu <yuegu1993@gmail.com>
2017-05-03 03:12:08 -0700

    user model unit tests

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+
+  describe 'validations' do
+    it { should validate_presence_of(:username) }
+    it { should validate_presence_of(:password_digest) }
+    it { should validate_presence_of(:session_token) }
+    it { should validate_length_of(:password).is_at_least(6) }
+  end
+
+  describe 'associations' do
+    it { should have_many(:anime_libraries) }
+    it { should have_many(:user_animes) }
+    it { should have_many(:animes).through(:user_animes) }
+  end
+
+  describe 'class methods' do
+    # user = User.create({username:"Guest", password:"password"})
+
+    describe '::find_by_credentials' do
+      it "should return nil when user doesn't exist" do
+        expect(User.find_by_credentials('user1', 'password')).to eq(nil)
+      end
+
+      it "should return nil when password is invalid" do
+        expect(User.find_by_credentials('Guest', 'passwor')).to eq(nil)
+      end
+
+      it "should return user when both username and password is correct" do
+        expect(User.find_by_credentials('Guest', 'password')).to_not eq(nil)
+      end
+    end
+  end
+
+end
