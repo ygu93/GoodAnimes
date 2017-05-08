c15c1e043ffa1ea4efe8e2f94a2f9f0ff35a3805
Yue Gu <yuegu1993@gmail.com>
2017-05-08 15:40:46 -0700

    refactor animes controller tests

--- spec/controllers/animes_controller_spec.rb
+++ spec/controllers/animes_controller_spec.rb
@@ -13,24 +13,38 @@ RSpec.describe Api::AnimesController, :type => :controller do
 
   describe "POST #create" do
     context "with valid params" do
-      it "successfully creates anime" do
+      before(:each) do
         post :create, format: :json, params: { anime: { title: "Fate Zero",
-                                                        synopsis: "Holy Grail War",
-                                                        start_date: "2015-04-03",
-                                                        end_date: "2015-08-03",
-                                                        image: "fake.com",
-                                                        score: 10,
-                                                        episodes: 26,
-                                                        media_type: "TV",
-                                                        status: "Finished Airing"  } }
+          synopsis: "Holy Grail War",
+          start_date: "2015-04-03",
+          end_date: "2015-08-03",
+          image: "fake.com",
+          score: 10,
+          episodes: 26,
+          media_type: "TV",
+          status: "Finished Airing"  } }
+        end
+      it "successfully creates anime" do
         expect(response).to be_success
       end
     end
   end
 
   describe "GET #show" do
+    before(:each) do
+      post :create, format: :json, params: { anime: { title: "Fate Stay Night",
+        synopsis: "Holy Grail War",
+        start_date: "2015-04-03",
+        end_date: "2015-08-03",
+        image: "fake.com",
+        score: 10,
+        episodes: 26,
+        media_type: "TV",
+        status: "Finished Airing"  } }
+      end
     it "successfully shows anime" do
-      get :show, format: :json, params: { id: 1 }
+      anime = Anime.find_by_title("Fate Stay Night")
+      get :show, format: :json, params: { id: anime.id }
       expect(response).to be_success
       expect(response).to render_template(:show)
     end
